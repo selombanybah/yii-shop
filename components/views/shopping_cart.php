0245db92e7821658d4dd222b4640652a55557cdb
trupke.k@googlemail.com <trupke.k@googlemail.com@382c672e-b228-9f00-e634-efad10a08afb>
2011-03-02 14:18:33 +0000

    extend shopping cart widget with css classes and ids

--- components/views/shopping_cart.php
+++ components/views/shopping_cart.php
@@ -1,24 +1,30 @@
-<?php
-
-if($products) {
-	$sum_price = 0;
-	echo '<table class="cart">';	
-	foreach($products as $position) { 
-		$model = Products::model()->findByPk($position['product_id']);
-		printf('<tr><td>%s</td><td>%s</td><td>%s %s</td>',
-				$position['amount'],
-				$model->title,
-				$position['amount'] * $model->getPrice(@$position['Variations']),
-				Shop::module()->currencySymbol
-				);
-		$sum_price += (float) $position['amount']* $model->getPrice(@$position['Variations']);
-	}
-	echo '</table>';	
-
-
-	printf('%s %s %s',
-			Shop::t('Price total:'),
-			$sum_price,
-			Shop::module()->currencySymbol);
-}
-?>
+<div id="shopping-cart">
+	<div id="shopping-cart-content">
+		<?php
+        if($products) {
+            $sum_price = 0;
+			echo '<h3>'.CHtml::link(Shop::t('Shopping cart'), array('//shop/shoppingCart/view')).'</h3>';
+            echo '<table cellpadding="0" cellspacing="0">';	
+            foreach($products as $position) { 
+                $model = Products::model()->findByPk($position['product_id']);
+                printf('<tr><td class="cart-left">%s</td><td class="cart-middle">%s</td><td class="cart-right">%s %s</td></tr>',
+                        $position['amount'],
+                        $model->title,
+                        $position['amount'] * $model->getPrice(@$position['Variations']),
+                        Shop::module()->currencySymbol
+                        );
+                $sum_price += (float) $position['amount']* $model->getPrice(@$position['Variations']);
+            }
+           	
+        
+        
+            printf('<tr><td colspan="2" class="cart-left cart-sum"><strong>%s</strong></td><td class="cart-sum cart-right">%s %s</td></tr>',
+                    Shop::t('Price total:'),
+                    $sum_price,
+                    Shop::module()->currencySymbol);
+			 echo '</table>';
+        }
+        ?>
+	</div>
+    <div id="shopping-cart-footer"></div>
+</div>
