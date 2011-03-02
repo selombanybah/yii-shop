bc5b65d595a61ff13865a522dc2bb917a7a14e80
trupke.k@googlemail.com <trupke.k@googlemail.com@382c672e-b228-9f00-e634-efad10a08afb>
2011-03-02 14:18:33 +0000

    extend shopping cart widget with css classes and ids

--- trunk/components/ShoppingCartWidget.php
+++ trunk/components/ShoppingCartWidget.php
@@ -4,14 +4,12 @@ Yii::import('zii.widgets.CPortlet');
 
 class ShoppingCartWidget extends CPortlet {
 	public function	init() {
-		$this->title = CHtml::link(Shop::t('Shopping cart'), array('//shop/shoppingCart/view'));
 		return parent::init();
 	}
 
 	public function	run() {
 		$this->render('shopping_cart', array('products' =>
 			Shop::getCartContent()));
-
 		return parent::run();
 	}
 
