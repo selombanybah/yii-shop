b294df0b7bfb28bda1f39e08f45efa0ede91eadc
thyseus <thyseus@382c672e-b228-9f00-e634-efad10a08afb>
2011-03-03 16:01:02 +0000

    shopping cart widget layout

--- trunk/components/views/shopping_cart.php
+++ trunk/components/views/shopping_cart.php
@@ -1,30 +1,32 @@
 <div id="shopping-cart">
-	<div id="shopping-cart-content">
-		<?php
-        if($products) {
-            $sum_price = 0;
+<div id="shopping-cart-content">
+<?php
+if($products) {
+	$sum_price = 0;
+	echo '<h3>'.CHtml::link(Shop::t('Shopping cart'), array(
+				'//shop/shoppingCart/view')) . '</h3>';
 
-						echo '<table cellpadding="0" cellspacing="0">';	
-            foreach($products as $position) { 
-                $model = Products::model()->findByPk($position['product_id']);
-                printf('<tr><td class="cart-left">%s</td><td class="cart-middle">%s</td><td class="cart-right">%s %s</td></tr>',
-                        $position['amount'],
-                        $model->title,
-                        Shop::priceFormat($position['amount'] * $model->getPrice(@$position['Variations'])),
-                        Shop::module()->currencySymbol
-                        );
-                $sum_price += (float) $position['amount']* $model->getPrice(@$position['Variations']);
-            }
-           	
-        
-        
-            printf('<tr><td colspan="2" class="cart-left cart-sum"><strong>%s</strong></td><td class="cart-sum cart-right">%s %s</td></tr>',
-                    Shop::t('Price total:'),
-                    Shop::priceFormat($sum_price),
-                    Shop::module()->currencySymbol);
-			 echo '</table>';
-        }
-        ?>
-	</div>
-    <div id="shopping-cart-footer"></div>
+	echo '<table cellpadding="0" cellspacing="0">';	
+	foreach($products as $position) { 
+		$model = Products::model()->findByPk($position['product_id']);
+		printf('<tr><td class="cart-left">%s</td><td class="cart-middle">%s</td><td class="cart-right">%s %s</td></tr>',
+				$position['amount'],
+				$model->title,
+				Shop::priceFormat($position['amount'] * $model->getPrice(@$position['Variations'])),
+				Shop::module()->currencySymbol
+				);
+		$sum_price += (float) $position['amount']* $model->getPrice(@$position['Variations']);
+	}
+
+
+
+	printf('<tr><td colspan="2" class="cart-left cart-sum"><strong>%s</strong></td><td class="cart-sum cart-right">%s %s</td></tr>',
+			Shop::t('Price total:'),
+			Shop::priceFormat($sum_price),
+			Shop::module()->currencySymbol);
+	echo '</table>';
+}
+?>
+</div>
+<div id="shopping-cart-footer"></div>
 </div>
